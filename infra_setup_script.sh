c881cf031951a6519b06a0e81bbe146cc3523672
Dömötör D. Lugosi <lugalaci@gmail.com>
2020-11-07 19:16:45 +0100

    Update infra_setup_script.sh

--- infra_setup_script.sh
+++ infra_setup_script.sh
@@ -10,8 +10,3 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install -y docker.io
 sudo apt-get install -y docker-compose
-
-git clone https://github.com/lugosidomotor/Prometheus-Grafana.git
-
-cd /home/ubuntu/Prometheus-Grafana/infra
-sudo docker-compose up -d
