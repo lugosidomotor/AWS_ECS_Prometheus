c83126bc0deb4318bcbca13815cb5422a62ccf7e
Dömötör D. Lugosi <lugalaci@gmail.com>
2020-11-09 09:49:31 +0100

    Update infra_setup_script.sh

--- infra_setup_script.sh
+++ infra_setup_script.sh
@@ -12,6 +12,8 @@ sudo apt-get install -y docker.io
 sudo apt-get install -y docker-compose
 sudo apt install -y python3-pip
 sudo apt  install -y awscli
-pip3 install -y boto3
-pip3 install -y awscli
+sudo apt  install golang-go
+sudo apt  install gccgo-go
+pip3 install boto3
+pip3 install awscli
 
