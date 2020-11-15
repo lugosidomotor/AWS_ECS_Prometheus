ee83ce7929d038fbe38fdf31ee5ffbfdcc3aa356
Dömötör D. Lugosi <lugalaci@gmail.com>
2020-11-15 19:47:12 +0100

    Update infra_setup_script.sh

--- infra_setup_script.sh
+++ infra_setup_script.sh
@@ -17,7 +17,11 @@ sudo apt  install -y awscli
 pip3 install boto3
 pip3 install awscli
 
-git clone https://github.com/teralytics/prometheus-ecs-discovery.git
-cd prometheus-ecs-discovery/
-sudo docker build -t aws-ser .
-sudo docker run -v /home/ubuntu/AWS_ECS_Prometheus/prometheus_files:/file -e AWS_REGION='us-east-1' -e AWS_ACCESS_KEY_ID='' -e AWS_SECRET_ACCESS_KEY='' aws-ser -config.write-to /file 
+git clone https://github.com/lugosidomotor/AWS_ECS_Prometheus.git
+cd AWS_ECS_Prometheus/
+sudo docker-compose up
+
+#git clone https://github.com/teralytics/prometheus-ecs-discovery.git
+#cd prometheus-ecs-discovery/
+#sudo docker build -t aws-ser .
+#sudo docker run -v /home/ubuntu/AWS_ECS_Prometheus/prometheus_files:/file -e AWS_REGION='us-east-1' -e AWS_ACCESS_KEY_ID='' -e AWS_SECRET_ACCESS_KEY='' aws-ser -config.write-to /file 
